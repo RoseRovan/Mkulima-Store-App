f4c6c4185f1acf4659d7ef45b05d14977c8c0d87
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-09-01 20:08:24 +0300

    linked to js and css and modified db

--- src/main.js
+++ src/main.js
@@ -11,3 +11,28 @@ document.addEventListener("DOMContentLoaded", () => {
           .then((response) => response.json())
           .then((data) => equipmentData(data));
       }
+      function equipmentData(data) {
+        data.forEach((data) => {
+          const list = document.createElement("li");
+          list.style.fontSize = "18px";
+          list.innerText = data.name;
+          equipmentUl.appendChild(list);
+    
+          list.addEventListener("click", () => {
+            equipmentElemName.textContent = data.name;
+            equipmentElemName.style.color = "red";       
+            equipmentElemImage.setAttribute("src", data.image_url);
+            equipmentElemDescription.textContent = data.description;
+            equipmentElemDescription.style.fontSize = "18px"
+          });
+        });
+      }
+      const equipmentRatings = document.getElementById('rating-list');
+      document.querySelector('#rating-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        let text = document.querySelector('#rating-form textarea')
+        let listRating = document.createElement('li');
+        listRating.innerText = text.value;
+        equipmentRatings.appendChild(listRating);
+        document.querySelector('#rating-form').reset();
+    })

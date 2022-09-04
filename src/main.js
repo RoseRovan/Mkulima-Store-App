792bb2573d1077450984735aef48e22c8386cddf
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-09-04 14:27:21 +0300

    feat-styled rating form

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-const equipmentUl = document.getElementById("equipment-list");
-const equipmentElemName = document.getElementById("equipment-name");
-const equipmentElemImage = document.getElementById("equipment-image");
-const equipmentElemDescription = document.getElementById("equipment-description");
-document.addEventListener("DOMContentLoaded", () => {
-    getEquipmentData();
-      });
-      //fetch function
-      function getequipmentData() {
-        fetch("http://localhost:3000/equipments")
-          .then((response) => response.json())
-          .then((data) => equipmentData(data));
-      }
-      function equipmentData(data) {
-        data.forEach((data) => {
-          const list = document.createElement("li");
-          list.style.fontSize = "18px";
-          list.innerText = data.name;
-          equipmentUl.appendChild(list);
-    
-          list.addEventListener("click", () => {
-            equipmentElemName.textContent = data.name;
-            equipmentElemName.style.color = "red";       
-            equipmentElemImage.setAttribute("src", data.image_url);
-            equipmentElemDescription.textContent = data.description;
-            equipmentElemDescription.style.fontSize = "18px"
-          });
-        });
-      }
-      const equipmentRatings = document.getElementById('rating-list');
-      document.querySelector('#rating-form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        let text = document.querySelector('#rating-form textarea')
-        let listRating = document.createElement('li');
-        listRating.innerText = text.value;
-        equipmentRatings.appendChild(listRating);
-        document.querySelector('#rating-form').reset();
+const hostUrl = "http://localhost:3000";
+
+function getAndLoadAllEquipments(){
+    fetch(`${hostUrl}/equipments`).then(resp=>resp.json()).then(equipments=>{
+        document.getElementById('equipment-list').innerHTML = equipments
+        .map(equipment=>`<li onClick="getAndLoadEquipmentDetails(${equipment.id})">${equipment.name}</li>`)
+        .join('');
     })
+}
+
+function getAndLoadEquipmentDetails(equipmentId){
+    fetch(`${hostUrl}/equipments${equipmentId}`).then(resp=>resp.json()).then(equipment=>{
+        console.log(beer);
+        document.getElementById('equipment-name').innerHTML = equipment.name;
+        document.getElementById('equipment-image').src = equipment.image_url;
+        document.getElementById('equipment-description').innerHTML = equipment.description;
+        document.getElementById('rating-list').innerHTML = equipment.ratings.map(rating=>`<li>${rating}</li>`).join('');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    getAndLoadAllEquipments();
+    getAndLoadEquipmentDetails(1);
+
+    document.getElementById('description-form').addEventListener('submit', (evt)=>{
+        evt.preventDefault();
+        const form = evt.target;
+        document.getElementById('equipment-description').innerHTML = form.description.value;
+        form.reset();
+    });
+
+    document.getElementById('rating-form').addEventListener('submit', evt=>{
+        evt.preventDefault();
+        const form = evt.target;
+        document.getElementById('rating-list').innerHTML += `<li>${form.rating.value}</li>`;
+        form.reset();
+    })
+})
